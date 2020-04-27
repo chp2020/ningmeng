928b0f365c578ee2c08513a3d1ca8ca6d99e4d30
chp <chp20200228@gmail.com>
2020-04-27 17:17:19 +0800

    first

---
+++ App.vue
@@ -0,0 +1,32 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	.page {
+		padding: 0 30rpx;
+		background-image: url(static/images/tab/tab.png);
+		background-size: 100% 406rpx;
+		background-repeat: no-repeat;
+		padding-bottom: 200rpx;
+		background-color: #FFFFFF;
+		.tab-bg {
+		}
+	}
+	
+	::-webkit-scrollbar {
+	    display: none;
+	}
+	
+</style>
